b0005d05c75bc51783c4950f7bbaa9e3e346d8ad
Cameron Chandler <camchandler1@gmail.com>
2022-06-27 09:36:26 +1000

    Added Setup Instructions

--- hyperlink_remover.py
+++ hyperlink_remover.py
@@ -10,58 +10,79 @@ import re
 import pdfrw
 
 # Change ROOTDIR to be the path to the `modules` folder
-ROOTDIR = r'C:\Computer\var\www\modules'
+ROOTDIR = '/var/www/modules'
 
 HTML_FILETYPES = ['html', 'htm']
 PDF_FILETYPES = ['pdf']
-
-count = sum(1 for _, _, files in os.walk(ROOTDIR))
 
 def clean_html(html: str) -> str:
     # Find every instance of href="http..." and delete entire href
     return re.sub('href *= *"http[^"<>]*"', '', html)
 
-def clean_pdf(pdf: 'pdf') -> 'pdf':
+def clean_pdf(pdf: 'pdf') -> tuple['pdf', bool]:
     ''' Takes pdf, creates new pdf and copies pages (but without URLs) '''
     new_pdf = pdfrw.PdfWriter()
+    is_changed = False
 
     for page in pdf.pages:
         # Links are in Annots, but some pages don't have links so Annots returns None
         for annot in page.Annots or []:
-
-            old_url = annot.A.URI
-            new_url = pdfrw.objects.pdfstring.PdfString('()')
-            annot.A.URI = new_url
+            try:
+                old_url = annot.A.URI
+                new_url = pdfrw.objects.pdfstring.PdfString('()')
+                annot.A.URI = new_url
+                is_changed = True
+            except:
+                pass
 
         new_pdf.addpage(page)
         
-    return new_pdf
+    return new_pdf, is_changed
 
+def desc(num_files_screened, num_files_changed): 
+    return f'Changed {num_files_changed} out of {num_files_screened} files'
+    
 def main():
+    num_files_screened = 0
+    num_files_changed  = 0
+    
+    progress_bar = tqdm(os.walk(ROOTDIR))
     # Walk through every file recursively from root directory
-    for subdir, dirs, files in tqdm(os.walk(ROOTDIR), total=count):
+    for subdir, dirs, files in progress_bar:
+        progress_bar.set_description(desc(num_files_screened, num_files_changed))
         for file in files:
-            path = os.path.join(subdir, file)
-            new_path = os.path.join(subdir, file)
-            
-            # If file is HTML-like, then clean up the links and overwrite file
-            if file.split('.')[-1] in HTML_FILETYPES:
-                with open(path, 'r', encoding='ISO-8859-1') as fpin:
-                    html = fpin.read()
+            try:
+                num_files_screened += 1
+                progress_bar.set_description(desc(num_files_screened, num_files_changed))
 
-                clean = clean_html(html)
-                if len(html) == len(clean):
-                    continue
+                path = os.path.join(subdir, file)
+                new_path = os.path.join(subdir, file)
 
-                with open(new_path, 'w') as fpout:
-                    fpout.write(clean)
-                    
-            # If file is PDF, then clean up the links and overwrite file
-            elif file.split('.')[-1] in PDF_FILETYPES:
-                pdf = pdfrw.PdfReader(path)
+                # If file is HTML-like, then clean up the links and overwrite file
+                if file.split('.')[-1] in HTML_FILETYPES:
+                    with open(path, 'r', encoding='ISO-8859-1') as fpin:
+                        html = fpin.read()
 
-                clean = clean_pdf(pdf)
-                clean.write(new_path)
+                    clean = clean_html(html)
+                    if len(html) == len(clean):
+                        continue
+                    num_html_changed += 1
+                    progress_bar.set_description(desc(num_files_screened, num_files_changed))
+
+                    with open(new_path, 'w', encoding='ISO-8859-1') as fpout:
+                        fpout.write(clean)
+
+                # If file is PDF, then clean up the links and overwrite file
+                elif file.split('.')[-1] in PDF_FILETYPES:
+                    pdf = pdfrw.PdfReader(path)
+
+                    clean, is_changed = clean_pdf(pdf)
+                    clean.write(new_path)
+
+                    num_files_changed += is_changed
+                    progress_bar.set_description(desc(num_files_screened, num_files_changed))
+            except:
+                pass
                 
 if __name__ == "__main__":
     main()
